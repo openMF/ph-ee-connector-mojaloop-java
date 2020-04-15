3a298a44db455e6831bdac4fa452fc31d021b1d9
Zoltan Nebli <zoltan.nebli@dpc.hu>
2020-04-15 13:11:07 +0200

    add objectmapper extensions

--- src/main/java/org/mifos/connector/mojaloop/MojaloopConnectorApplication.java
+++ src/main/java/org/mifos/connector/mojaloop/MojaloopConnectorApplication.java
@@ -3,6 +3,8 @@ package org.mifos.connector.mojaloop;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.camel.Processor;
 import org.mifos.connector.mojaloop.properties.PartyProperties;
 import org.slf4j.Logger;
@@ -27,9 +29,13 @@ public class MojaloopConnectorApplication {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper()
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        return objectMapper
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
     @Bean
